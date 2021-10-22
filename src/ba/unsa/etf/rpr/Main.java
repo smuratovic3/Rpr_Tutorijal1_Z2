0986547b8936a93db0cc0291374156f662bdd517
smuratovic3 <smuratovic3@etf.unsa.ba>
2021-10-22 12:44:44 +0200

    Drugi zadatak, tutorijal 1

--- src/ba/unsa/etf/rpr/Main.java
+++ src/ba/unsa/etf/rpr/Main.java
@@ -1,8 +1,29 @@
 package ba.unsa.etf.rpr;
 
+import java.util.Scanner;
+
 public class Main {
+    static int SumaCifara(int n) {
+        int suma = 0, m;
+        while (n > 0) {
+            m = n % 10;
+            suma += m;
+            n = n / 10;
+        }
+        return suma;
+    }
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+        Scanner ulaz = new Scanner(System.in);
+        System.out.println("Unesi broj n: ");
+        int n = ulaz.nextInt();
+        System.out.println("Brojevi između 1 i n koji su djeljivi sa sumom svojih cifara su: ");
+        for (int i = 1; i <= n; i++) {
+            if (i % SumaCifara(n) == 0) {
+                System.out.println(i);
+            }
+
+        }
     }
 }
